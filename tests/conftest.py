3ed4ae9bcb75f82cc9394e300dfd113dba109fe6
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-12-19 22:47:28 +0300

    Create conftest.py
    1. Redefine event_loop fixture from pytest-asyncio
    2. Create a fixture for retrieving test http connector
    3. Implement retrieving api keys from local file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,32 @@
+import asyncio
+
 import pytest
 
 from aiobitmex.http import BitmexHTTP
 
+try:
+    with open('tests/keys.txt', 'r') as f:
+        API_KEY = f.readline()[:24]
+        API_SECRET = f.readline()[:48]
+except FileNotFoundError:
+    raise Exception('API key and API secret were not found in tests/keys.txt')
 
-@pytest.fixture
+
+# Redefine pytest-asyncio fixture with changing scope from 'function' to 'session'
+@pytest.fixture(scope='session')
+def event_loop():
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope='session')
 async def test_http_conn():
-    test_conn = BitmexHTTP(base_url='https://testnet.bitmex.com/api/v1', symbol='XBTUSD', api_key='', api_secret='')
+    test_conn = BitmexHTTP(
+        base_url='https://testnet.bitmex.com/api/v1',
+        symbol='XBTUSD',
+        api_key=API_KEY,
+        api_secret=API_SECRET
+    )
     yield test_conn
     await test_conn.session.close()
